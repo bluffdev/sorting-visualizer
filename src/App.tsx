f88aa369bf211e0d5ddd0160d8d56e14aa2d3336
Brandon Rubio <brubio3724@gmail.com>
2022-12-20 21:22:02 -0500

    Added buttons and bubble sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 function generateArray() {
+  let values = new Set<number>();
   let array = new Array<number>(50);
   for (let i = 0; i < array.length; i++) {
-    array[i] = Math.floor(Math.random() * (100 - 25) + 25);
+    let newValue = Math.floor(Math.random() * (100 - 25) + 25);
+    while (values.has(newValue)) {
+      newValue = Math.floor(Math.random() * (100 - 25) + 25);
+    }
+    values.add(newValue);
+    array[i] = newValue;
   }
   return array;
 }
@@ -11,13 +17,79 @@ function generateArray() {
 export default function App() {
   let [array, setArray] = useState(generateArray());
 
+  function bubbleSort(a: number[]) {
+    for (let i = 0; i < a.length; i++) {
+      for (let j = 0; j < a.length - i - 1; j++) {
+        if (a[j] > a[j + 1]) {
+          let temp = a[j];
+          a[j] = a[j + 1];
+          a[j + 1] = temp;
+        }
+      }
+    }
+    return new Array(...a);
+  }
+
   return (
-    <div className="flex">
-      {array.map((a) => (
-        <div className="border m-2 w-[25px]" style={{ height: `${a * 5}px` }}>
-          a
-        </div>
-      ))}
-    </div>
+    <>
+      <button
+        className="border-1 p-2"
+        onClick={() => {
+          setArray(bubbleSort(array));
+        }}
+      >
+        Sort
+      </button>
+      <button
+        className="border-1 p-2"
+        onClick={() => {
+          setArray(generateArray());
+        }}
+      >
+        Reset
+      </button>
+      <div className="flex">
+        {array.map((value) => (
+          <div
+            className="border m-2 w-[25px]"
+            style={{ height: `${value * 5}px` }}
+            key={value}
+          >
+            a
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
+// let nextId = 0;
+
+// export default function List() {
+//   const [name, setName] = useState("");
+//   const [artists, setArtists] = useState(
+//     new Array<{ id: number; name: string }>()
+//   );
+
+//   return (
+//     <>
+//       <h1>Inspiring sculptors:</h1>
+//       <input value={name} onChange={(e) => setName(e.target.value)} />
+//       <button
+//         onClick={() => {
+//           setName("");
+//           artists.push({
+//             id: nextId++,
+//             name: name,
+//           });
+//         }}
+//       >
+//         Add
+//       </button>
+//       <ul>
+//         {artists.map((artist) => (
+//           <li key={artist.id}>{artist.name}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
